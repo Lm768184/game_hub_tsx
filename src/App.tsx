110d1226fc1b55f98d3b0a3efcdb45b5c2c583f1
laurence0202 <laurence0202@users.noreply.github.com>
2024-01-09 19:49:45 +1100

    added respnosive chakra layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from '@chakra-ui/react'
 
 function App() {
-  return <Button colorScheme='blue'>Button</Button>
+  return <Grid templateAreas={{
+    base: `"nav" "main"`,
+  }}
+
+  // gridTemplateRows={'50px 1fr'}
+  // gridTemplateColumns={'150px 1fr'}
+  // h='70vw'
+  // gap='1'
+  // color='blackAlpha.700'
+  // fontWeight='bold'
+  >
+    
+    <GridItem gridArea="nav" bg="red.300">nav</GridItem>
+    <Show above="lg"><GridItem gridArea="aside" bg="blue.300">aside</GridItem> </Show>
+    <GridItem gridArea="main" bg="green.300">main</GridItem>
+  </Grid>
 }
 
 export default App
